962b1c7948219a492af3c354dc5d0fb2dda797ea
AlexanderOcoro <clasestic2014@gmail.com>
2023-11-13 16:43:03 -0500

    primer commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import Card from './Card';
+
+export default function Cards({ characters }) {
+   return (
+   <div>
+      { characters.map(characters => (
+         <Card 
+            key={characters.id}
+            id={characters.id}
+            name={characters.name}
+            status={characters.status}
+            species={characters.species}
+            gender={characters.gender}
+            origin={characters.origin.name}
+            image={characters.image}
+            onClose={() => window.alert('Emulamos que se cierra la card')}
+         />
+         ))
+      }
+   </div>
+   );
+}
